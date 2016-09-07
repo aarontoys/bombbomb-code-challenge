45a8bf91424d00030a0b7d24c0bcc881c1c750d1
Aaron Toys <aarontoys@gmail.com>
2016-09-07 10:53:38 -0600

    create route that makes API request to search youtube

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var rp = require('request-promise');
+var apikey = process.env.SKEY
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/funnyVid/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  console.log('line8: ',apikey);
+  var options = {
+    uri: 'https://www.googleapis.com/youtube/v3/search',
+    qs: {
+      part: 'snippet',
+      maxResults: 50,
+      q: req.params.q || 'funny fail',
+      key: apikey
+    },
+    json: true
+  };
+
+  rp(options)
+    .then(function (result) {
+      res.status(200).json({
+        status: 'success',
+        myResult: result
+      });
+      console.log(result);
+    })
+    .catch(function (error) {
+      return next(error);
+    })
 });
 
 module.exports = router;
